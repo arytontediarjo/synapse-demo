b3c78f6530b1f8d6a66f25e5d62503a08a7ac73a
arytontediarjo <atediarjo@gmail.com>
2021-03-17 12:46:15 -0700

    upload new .rmd

--- R/create_manifest.R
+++ R/create_manifest.R
@@ -3,9 +3,11 @@ library(synapser)
 library(data.table)
 library(here)
 
-manifest <- fread("example_manifest_template.tsv")
-manifest$path <- purrr::map(list.files("data"), ~(glue::glue(file.path(here(),.x))))
-manifest$parent <- "syn25257395"
-manifest$name <- "bulk upload test"
-manifest$used <- ""
-manifest$executed <- ""
+manifest <- tibble::tibble(
+    path = purrr::map_chr(list.files("data"), ~(glue::glue(file.path(here(),"data",.x)))),
+    parent = "syn25257395",
+    name = list.files("data"),
+    used = "syn23555388",
+    executed = "https://github.com/arytontediarjo/synapse-demo/blob/main/Notebook/file_upload.Rmd",
+    dataType = "samples-demo"
+) %>% write.table(., "manifest.tsv", sep = "\t", row.names = FALSE, quote = FALSE)
